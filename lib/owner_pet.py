f473e60580e69c97c32bfaab1bb8b2d0bacba3d0
Ashley Natasha <ashleyotsiula@gmail.com>
2024-12-06 21:03:29 +0300

    one to one

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,31 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all=[]
+
+    def __init__(self,name, pet_type, owner="Jeff"):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Invalid pet type")
+        Pet.all.append(self)
+
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name = name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        
+        else:
+            raise Exception("Is not an instance.")
+        
+    def get_sorted_pets(self):
+        pet_list = sorted(self.pets(),key = lambda pet: pet.name)
+        return pet_list
